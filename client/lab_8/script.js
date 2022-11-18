f3638e840f68aba056020f8990d2f816d89e00ce
Steven Rao <60757809+Srealbyte@users.noreply.github.com>
2022-11-17 20:18:34 -0500

    Lab 8 Completed

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -11,8 +11,8 @@
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 */
 function getRandomIntInclusive(min, max) {
-  min1 = Math.ceil(min);
-  max1 = Math.floor(max);
+  const min1 = Math.ceil(min);
+  const max1 = Math.floor(max);
   return Math.floor(Math.random() * (max1 - min1 + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
@@ -81,7 +81,32 @@ function filterList(list, fitlerInputValue) {
     return lowerCaseName.includes(lowerCaseQuery);
   });
 }
-
+function initMap() {
+  //38.9897° N, 76.9378° W
+  console.log('initMap');
+  const map = L.map('map').setView([38.9897, -76.9378], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  return map;
+}
+function markerPlacer(array, map) {
+  console.log('markerPlace', array);
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+  array.forEach((item, index) => {
+    const {coordinates} = item.geocoded_column_1;
+    console.log(item);
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    if (index === 0) {
+      map.setView([coordinates[1], coordinates[0]], 9);
+    }
+  });
+}
 async function mainEvent() {
   /*
         ## Main Event
@@ -94,6 +119,7 @@ async function mainEvent() {
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
   const submit = document.querySelector('#get-resto'); // get a reference to your submit button
   const loadAnimation = document.querySelector('.lds-ellipsis');
+  const pageMap = initMap();
   submit.style.display = 'none'; // let your submit button disappear
 
   /*
@@ -103,7 +129,6 @@ async function mainEvent() {
        */
   const results = await fetch('/api/foodServicePG');
   const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
-
   /*
         Below this comment, we log out a table of all the results using "dot notation"
         An alternate notation would be "bracket notation" - arrayFromJson["data"]
@@ -127,12 +152,14 @@ async function mainEvent() {
     submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
     loadAnimation.classList.remove('lds-ellipsis');
     loadAnimation.classList.add('lds-ellipsis_hidden');
+    let currentList = [];
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
     // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
     form.addEventListener('input', (event) => {
       console.log(event.target.value);
       const newFilterList = filterList(currentList, event.target.value);
       injectHTML(newFilterList);
+      markerPlacer(newFilterList, pageMap);
     });
     form.addEventListener('submit', (submitEvent) => {
       // This is needed to stop our page from changing to a new URL even though it heard a GET request
@@ -143,6 +170,7 @@ async function mainEvent() {
       console.log(currentList);
       // And this function call will perform the "side effect" of injecting the HTML list for you
       injectHTML(currentList);
+      markerPlacer(currentList, pageMap);
 
       // By separating the functions, we open the possibility of regenerating the list
       // without having to retrieve fresh data every time
